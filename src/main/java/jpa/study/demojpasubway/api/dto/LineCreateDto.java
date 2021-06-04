7f7633cfdab13f3831c818bc5649f5e32dff4de4
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-04 23:16:04 +0900

    add LineCreateDto

---
+++ src/main/java/jpa/study/demojpasubway/api/dto/LineCreateDto.java
@@ -0,0 +1,22 @@
+package jpa.study.demojpasubway.api.dto;
+
+public class LineCreateDto {
+    private Integer lineNumber;
+    private String lineName;
+
+    public Integer getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(Integer lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public void setLineName(String lineName) {
+        this.lineName = lineName;
+    }
+}
