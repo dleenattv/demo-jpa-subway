64c1b1e78fdce15e54cd428a101f6358681cef7e
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-04 23:10:09 +0900

    add SubwayCreateDto

---
+++ src/main/java/jpa/study/demojpasubway/api/dto/SubwayCreateDto.java
@@ -0,0 +1,22 @@
+package jpa.study.demojpasubway.api.dto;
+
+public class SubwayCreateDto {
+    private String stationName;
+    private Integer lineNumber;
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public Integer getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(Integer lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+}
