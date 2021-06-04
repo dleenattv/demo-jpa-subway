9e35f3d2fd86117dda49dc332a6c6cd14b0e7683
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-05 00:31:56 +0900

    add findSubwayByStationname to SubwayRepo

--- src/main/java/jpa/study/demojpasubway/domain/subway/repository/SubwayRepository.java
+++ src/main/java/jpa/study/demojpasubway/domain/subway/repository/SubwayRepository.java
@@ -2,6 +2,9 @@ package jpa.study.demojpasubway.domain.subway.repository;
 
 import jpa.study.demojpasubway.domain.subway.entity.Subway;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubwayRepository extends JpaRepository<Subway, Long> {
+    Subway findSubwayByStationName(String stationName);
 }
