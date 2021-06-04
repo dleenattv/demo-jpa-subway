339a34ef72cf38d39b5fb171bd6e5fc88b2ba256
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-04 22:09:49 +0900

    add createSubway()

---
+++ src/main/java/jpa/study/demojpasubway/domain/subway/service/SubwayService.java
@@ -0,0 +1,21 @@
+package jpa.study.demojpasubway.domain.subway.service;
+
+import jpa.study.demojpasubway.domain.subway.entity.Subway;
+import jpa.study.demojpasubway.domain.subway.repository.SubwayRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubwayService {
+
+    private SubwayRepository subwayRepository;
+
+    public SubwayService(SubwayRepository subwayRepository) {
+        this.subwayRepository = subwayRepository;
+    }
+
+    public Subway createSubway(String stationName) {
+        Subway subway = new Subway(stationName);
+        subwayRepository.save(subway);
+        return subway;
+    }
+}
