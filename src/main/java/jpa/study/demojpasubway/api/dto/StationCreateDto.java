cee6cad3368eb56cf9363f6573779cf6b583bd78
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-05 11:24:37 +0900

    Create StationCreateDto

---
+++ src/main/java/jpa/study/demojpasubway/api/dto/StationCreateDto.java
@@ -0,0 +1,22 @@
+package jpa.study.demojpasubway.api.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class StationCreateDto {
+
+    @NotBlank
+    private String stationName;
+
+    @NotNull
+    private Integer lineNumber;
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public Integer getLineNumber() {
+        return lineNumber;
+    }
+
+}
