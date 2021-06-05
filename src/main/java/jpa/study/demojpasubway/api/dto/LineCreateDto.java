5b05e0a33fae518b1ef268cadec9278a491a4124
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-05 11:25:14 +0900

    Add validation in LineCreateDto

--- src/main/java/jpa/study/demojpasubway/api/dto/LineCreateDto.java
+++ src/main/java/jpa/study/demojpasubway/api/dto/LineCreateDto.java
@@ -1,12 +1,17 @@
 package jpa.study.demojpasubway.api.dto;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public class LineCreateDto {
 
+    @NotNull
     private Integer lineNumber;
+
+    @NotNull
     private String lineName;
-    private List<String> subwayNames;
+
+    private List<String> stationNames;
 
     public Integer getLineNumber() {
         return lineNumber;
