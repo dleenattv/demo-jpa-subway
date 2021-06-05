b80c0e0b61f817896be91427cf8af842b32d11e9
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-05 11:24:48 +0900

    Create StationController

---
+++ src/main/java/jpa/study/demojpasubway/api/controller/StationController.java
@@ -0,0 +1,46 @@
+package jpa.study.demojpasubway.api.controller;
+
+import jpa.study.demojpasubway.api.dto.StationCreateDto;
+import jpa.study.demojpasubway.api.dto.StationUpdateDto;
+import jpa.study.demojpasubway.domain.station.entity.Station;
+import jpa.study.demojpasubway.domain.station.service.StationService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StationController {
+
+    private final StationService stationService;
+
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
+    }
+
+    @PostMapping("/stations")
+    public Station createStation(@Valid StationCreateDto stationCreateDto) throws Exception {
+        return stationService.createStation(stationCreateDto);
+    }
+
+    @GetMapping("/stations")
+    public List<Station> findAllStation() {
+        return stationService.findAllStation();
+    }
+
+    @GetMapping("/stations/{id}")
+    public Optional<Station> findStationById(@PathVariable Long id) {
+        return stationService.findStationById(id);
+    }
+
+    @PutMapping("/stations/{id}")
+    public Station updateStation(@PathVariable Long id, @Valid StationUpdateDto stationUpdateDto) {
+        return stationService.updateStation(id, stationUpdateDto);
+    }
+
+    @DeleteMapping("/stations/{id}")
+    public void deleteStation(@PathVariable Long id) {
+        stationService.deleteStation(id);
+    }
+}
