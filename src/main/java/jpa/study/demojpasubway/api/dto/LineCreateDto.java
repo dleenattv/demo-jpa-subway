7ff4cc00b3c2cd587738dfd7f689ff0f543f0725
Junhee Ko <jko@rainist.com>
2021-06-05 10:54:44 +0900

    Add constructor in LineCreateDto

--- src/main/java/jpa/study/demojpasubway/api/dto/LineCreateDto.java
+++ src/main/java/jpa/study/demojpasubway/api/dto/LineCreateDto.java
@@ -8,11 +8,21 @@ public class LineCreateDto {
     private String lineName;
     private List<String> subwayNames;
 
+    public LineCreateDto(Integer lineNumber, String lineName, List<String> subwayNames) {
+        this.lineNumber = lineNumber;
+        this.lineName = lineName;
+        this.subwayNames = subwayNames;
+    }
+
     public Integer getLineNumber() {
         return lineNumber;
     }
 
     public String getLineName() {
         return lineName;
+    }
+
+    public List<String> getSubwayNames() {
+        return subwayNames;
     }
 }
