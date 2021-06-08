a14812653cfe000170211d3e5ffa62ea3ada0686
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-09 00:27:46 +0900

    Add findStationByLine

--- src/main/java/jpa/study/demojpasubway/domain/station/repository/StationRepository.java
+++ src/main/java/jpa/study/demojpasubway/domain/station/repository/StationRepository.java
@@ -1,11 +1,15 @@
 package jpa.study.demojpasubway.domain.station.repository;
 
+import jpa.study.demojpasubway.domain.line.entity.Line;
 import jpa.study.demojpasubway.domain.station.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
 
     Station findSubwayByStationName(String stationName);
+    List<Station> findStationsByLine(Line line);
 }
