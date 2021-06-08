1b705c3e301f8e3f4ac4596813e9aa4ecb31ccbe
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-09 00:27:04 +0900

    Add createLine/deleteLine logic

--- src/main/java/jpa/study/demojpasubway/domain/line/service/LineService.java
+++ src/main/java/jpa/study/demojpasubway/domain/line/service/LineService.java
@@ -3,29 +3,36 @@ package jpa.study.demojpasubway.domain.line.service;
 import jpa.study.demojpasubway.api.dto.LineCreateDto;
 import jpa.study.demojpasubway.domain.line.entity.Line;
 import jpa.study.demojpasubway.domain.line.repository.LineRepository;
+import jpa.study.demojpasubway.domain.station.entity.Station;
+import jpa.study.demojpasubway.domain.station.service.StationService;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class LineService {
 
     private final LineRepository lineRepository;
+    // 순환 참조 발생 (LineController -> LineService -> StationService)
+    private final StationService stationService;
 
-    public LineService(LineRepository lineRepository) {
+    public LineService(LineRepository lineRepository, StationService stationService) {
         this.lineRepository = lineRepository;
+        this.stationService = stationService;
     }
 
-    public Line createLine(LineCreateDto lineCreateDto) {
-        Line line = new Line(lineCreateDto.getLineNumber(), lineCreateDto.getLineName());
-        lineRepository.save(line);
+    public Line createLine(LineCreateDto lineCreateDto) throws Exception {
+        Line existLine = findLineByLineNumber(lineCreateDto.getLineNumber());
 
-        return line;
-    }
+        if (existLine == null) {
+            Line line = new Line(lineCreateDto.getLineNumber(), lineCreateDto.getLineName());
+            lineRepository.save(line);
 
-    public Optional<Line> findLineById(Long id) {
-        return lineRepository.findById(id);
+            return line;
+        } else {
+            throw new Exception("Line" + lineCreateDto.getLineNumber() + " already exists.");
+        }
     }
 
     public Line findLineByLineNumber(Integer lineNumber) {
@@ -34,6 +41,10 @@ public class LineService {
 
     public void deleteLine(Long id) {
         Line line = lineRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        List<Station> stations = stationService.findStationsByLineNumber(line.getLineNumber());
+        for (Station station : stations) {
+            stationService.deleteStation(station.getId());
+        }
         lineRepository.delete(line);
     }
 }
