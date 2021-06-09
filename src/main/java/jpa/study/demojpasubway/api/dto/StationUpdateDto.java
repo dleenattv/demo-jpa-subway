e3bd158276c2151bfd4141147d1f48c6b5181567
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-09 23:19:08 +0900

    Add getter in StationUpdateDto

--- src/main/java/jpa/study/demojpasubway/api/dto/StationUpdateDto.java
+++ src/main/java/jpa/study/demojpasubway/api/dto/StationUpdateDto.java
@@ -1,8 +1,5 @@
 package jpa.study.demojpasubway.api.dto;
 
-import com.sun.istack.Nullable;
-import jpa.study.demojpasubway.domain.line.entity.Line;
-
 import javax.validation.constraints.NotNull;
 
 public class StationUpdateDto {
@@ -13,14 +10,11 @@ public class StationUpdateDto {
     @NotNull
     private Integer lineNumber;
 
-    @Nullable
-    private Line line;
-
     public String getStationName() {
         return stationName;
     }
 
-    public Line getLine() {
-        return line;
+    public Integer getLineNumber() {
+        return lineNumber;
     }
 }
