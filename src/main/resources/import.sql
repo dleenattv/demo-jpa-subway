a2b018953eeab84a4ca3b28859ce145835c48341
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-09 23:19:15 +0900

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,8 @@
-insert into line (line_id, line_number, line_name) values (1, 2, '2호선');
-insert into station (station_id, station_name, line_id) values (1, '홍대', 1);
+insert into line (line_id, line_number, line_name) values (1, 1, '1호선');
+insert into line (line_id, line_number, line_name) values (2, 2, '2호선');
+insert into line (line_id, line_number, line_name) values (3, 3, '3호선');
+insert into station (station_id, station_name, line_id) values (1, '홍대', 2);
+insert into station (station_id, station_name, line_id) values (2, '신촌', 2);
+insert into station (station_id, station_name, line_id) values (3, '광명', 2);
+insert into station (station_id, station_name, line_id) values (4, '압구정', 3);
+insert into station (station_id, station_name, line_id) values (5, '시청', 1);
