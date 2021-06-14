9846f2449d44d92004661f3bd2a9e755f13b1daf
dleenattv <67464408+dleenattv@users.noreply.github.com>
2021-06-14 20:18:48 +0900

    Update findLineByLineNumber in LineRepository

--- src/main/java/jpa/study/demojpasubway/domain/line/repository/LineRepository.java
+++ src/main/java/jpa/study/demojpasubway/domain/line/repository/LineRepository.java
@@ -4,8 +4,10 @@ import jpa.study.demojpasubway.domain.line.entity.Line;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-    Line findLineByLineNumber(Integer lineNumber);
+    Optional<Line> findLineByLineNumber(Integer lineNumber);
 }
